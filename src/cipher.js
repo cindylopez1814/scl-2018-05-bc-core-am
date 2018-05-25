35e0ebaa7bbd1e200438fa071baa3cb3fcdc93be
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-05-25 13:00:54 -0400

    modificar textarea, comienzo index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,7 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
+    (x - 65 + n) % 26 + 65
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 /* Acá va tu código */
+const alph = [A,B,C,D,E,F,G,H,I,J,K,L,M,Ñ,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+
+for (let i = 0; i < alph.length; i++){
+    
+  
+}
