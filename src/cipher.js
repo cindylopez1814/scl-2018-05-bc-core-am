72bf399a3310f140157ef0c417763d604dad4e0f
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-05-30 22:23:12 -0400

    subiendo metodo decode

--- src/cipher.js
+++ src/cipher.js
@@ -2,13 +2,17 @@ window.cipher = {
   encode: () => {
     /* Acá va tu código */
     for (let i = 0; i < txtCod.length; i++){
-      let ascii= txtCod.charCodeAt (i);    
+      let ascii = txtCod.charCodeAt (i);    
       let code = ascii + offset;
-      let result = String.fromCharCode(code); 
-      return result;
+      return String.fromCharCode(code); 
     };
   },
   decode: () => {
     /* Acá va tu código */
+    for (let i = 0; i < txtDec.length; i++){
+      let asciiDec = txtDec.charCodeAt (i);
+      let codeDec = asciiDec - offset;
+      return String.fromCharCode(codeDec);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 /* Acá va tu código */
-
 // Declarar variables
-
 let txtCod = document.getElementById("txt1").value;
 let txtDec = document.getElementById("txt2").value;
 const btnCod = document.getElementById("button-cod");
 const btnDecode= document.getElementById("button-deco");
 const offset = document.getElementById("offset").value;
 const mnsj = document.getElementById ("root");
-
 // Funcionalidad Boton "Codificar"
   btnCod.addEventListener ("click", () => {
     mnsj.innerHTML = cipher.encode()
-  });
+  }); 
+// Funcionalidad Boton "Decodificar"
+
 
